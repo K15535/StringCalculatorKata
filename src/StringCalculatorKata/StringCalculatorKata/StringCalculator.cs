e385c9ecfe122d52b2c4d1e125fa390c10e3f99b
Leo PAOL <lpaol@lucca.fr>
2022-05-09 16:02:49 +0200

    Add Step 4

--- src/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
+++ src/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -4,29 +4,52 @@ public static class StringCalculator
 {
     public static int Add(string numbers)
     {
-        numbers = numbers.Replace("\n", ",");
+        string delimiter = ",";
 
-        if (numbers.StartsWith("//"))
-        {
-            return 8;
-        }
-        if (numbers.Contains(','))
-        {
-            string[] splittedNumbers = numbers.Split(',');
+        if (string.IsNullOrEmpty(numbers))
+            return 0;
 
-            if (splittedNumbers.Length > 0)
+        if (!numbers.Contains(delimiter) && !numbers.Contains("\n"))
+            return int.Parse(numbers);
+
+        List<string>? lines = null;
+        if (numbers.Contains("\n"))
+            lines = numbers.Split('\n').ToList();
+
+        if (lines != null && lines.Any())
+        {
+            int sum = 0;
+            
+            foreach (string line in lines)
             {
-                int sum = 0;
-
-                foreach (string number in splittedNumbers)
-                {
-                    sum += int.Parse(number);
-                }
-
-                return sum;
+                if (line.StartsWith("//"))
+                    delimiter = line.Substring(2);
+                else
+                    sum += GetSumFromString(line, delimiter);
             }
+
+            return sum;
+        }
+        
+        return GetSumFromString(numbers, delimiter);
+    }
+
+    private static int GetSumFromString(string line, string delimiter)
+    {
+        string[] splittedNumbers = line.Split(delimiter);
+
+        if (splittedNumbers.Length > 0)
+        {
+            int sum = 0;
+
+            foreach (string number in splittedNumbers)
+            {
+                sum += int.Parse(number);
+            }
+
+            return sum;
         }
 
-        return String.IsNullOrEmpty(numbers) ? 0 : int.Parse(numbers);
+        return int.Parse(line);
     }
 }
